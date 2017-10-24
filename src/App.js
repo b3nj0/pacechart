becd5078372a435d0dd1504f077d4c0c87397434
Benjamin Jenks <jenksb@gmail.com>
2017-10-24 19:27:00 +0100

    App.js: added page description

--- src/App.js
+++ src/App.js
@@ -266,6 +266,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Pace Chart</h1>
+        <p>5K, 10K, 1/2 Marathon, Marathon finish times given your expected pace.</p>
         <PaceChart />
       </div>
     );
